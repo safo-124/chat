4f57598e917e623408b13205ba42793f211d1f58
Safo Emmanuel <nanasafo122@gmail.com>
2023-08-27 17:28:46 +0000

    sixth commit

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -9,6 +9,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from 'react';
 import { FileUpload } from '@/components/file-upload';
+import axios from "axios"
+import { useRouter } from 'next/navigation';
 
 const formSchema = z.object({
     name:z.string().min(1, {
@@ -20,7 +22,9 @@ const formSchema = z.object({
 })
 
 export const InitialModal = () => {
-    const [isMounted, setIsMounted] = useState(false)
+    const [isMounted, setIsMounted] = useState(false);
+
+    const router = useRouter()
 
     useEffect(() => {
         setIsMounted(true);
@@ -35,7 +39,15 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            await axios.post("/api/servers", values);
+
+            form.reset();
+            router.refresh(); 
+            window.location.reload();
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     if (!isMounted) {
