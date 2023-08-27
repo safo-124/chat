037437aa2c121b93f9362a6f8fa181011382bb0a
Safo Emmanuel <nanasafo122@gmail.com>
2023-08-27 15:18:53 +0000

    Fifth commit

---
+++ components/file-upload.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { X } from "lucide-react"
+import Image from "next/image";
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css"
+
+interface FieldUploadProps {
+    onChange: (url?: string) => void;
+    value: string;
+    endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload = ({
+    onChange, value, endpoint
+}: FieldUploadProps) => {
+    const fileType = value?.split(".").pop();
+
+    if (value && fileType !=="pdf") {
+        return (
+            <div className="relative h-20 w-20">
+                <Image 
+                fill src={value} alt="Upload" className="rounded-full"/>
+                <button className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm" onClick={() => onChange("")} >
+                    <X className="h-4 w-4"/>
+                </button>
+            </div>
+        )
+    }
+    return (
+        <UploadDropzone 
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url)
+            }}
+            onUploadError={(error: Error) => {
+                console.log(error)
+            }}
+        />
+    )
+}
