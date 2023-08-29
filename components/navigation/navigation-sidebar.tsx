9510ee892bb3fd6fe92f4abcaa976b0f5ba486cf
Safo Emmanuel <nanasafo122@gmail.com>
2023-08-29 19:07:32 +0000

    tenth commit

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -5,6 +5,9 @@ import { redirect } from "next/navigation";
 import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -28,11 +31,23 @@ export const NavigationSidebar = async () => {
             <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"/> 
             <ScrollArea className="flex-1 w-full">
                 {servers.map((server) => (
-                    <div key={server.id}>
-                        {server.name}
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem 
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
                     </div>
                 ))}
             </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                    <ModeToggle/>
+                    <UserButton afterSignOutUrl="/" appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]"
+                        }
+                    }}/>
+            </div>
         </div>
     )
 }
