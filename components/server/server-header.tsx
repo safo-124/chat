6937de2ec6ef4e4c083600b0f5d74840ed7213d0
Safo Emmanuel <nanasafo122@gmail.com>
2023-09-03 19:20:33 +0000

    sixteen commit

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -44,7 +44,8 @@ export const ServerHeader = ({
                     </DropdownMenuItem>
                  )} 
                   {isAdmin && (
-                    <DropdownMenuItem className=" px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={() => onOpen("members", {server})}
+                    className=" px-3 py-2 text-sm cursor-pointer">
                         Manage Users
                         <User className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
