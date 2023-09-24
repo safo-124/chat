b05dde8631b7a2443953b1d5121f9d73a5571dea
Safo Emmanuel <nanasafo122@gmail.com>
2023-09-24 15:38:09 +0000

    46

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import { ChatItem } from "./chat-item";
 import { format} from "date-fns"
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
@@ -32,12 +33,16 @@ export const ChatMessages = ({
     paramKey, paramValue, type
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`
 
     const {
         data, fetchNextPage, hasNextPage, isFetchingNextPage, status
     } = useChatQuery({
         queryKey, apiUrl, paramKey, paramValue
-    })
+    });
+
+    useChatSocket({queryKey, addKey, updateKey});
 
     if (status === "loading") {
         return (
