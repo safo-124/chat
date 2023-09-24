9d846efa0d3da6fc1b58551b571774768367a11c
Safo Emmanuel <nanasafo122@gmail.com>
2023-09-24 06:35:42 +0000

    35

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Open_Sans } from 'next/font/google'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { SocketProvider } from '@/components/providers/socket-provider'
 
 const fonts = Open_Sans({ subsets: ['latin'] })
 
@@ -27,8 +28,10 @@ export default function RootLayout({
           <ThemeProvider attribute="class" defaultTheme="dark"
             enableSystem={false} storageKey="discord-theme"
           >
-            <ModalProvider/>
-            {children}
+            <SocketProvider>
+              <ModalProvider/>
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
